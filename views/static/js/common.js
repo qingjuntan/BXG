4d70586652658d75a6670207bf9b1ca749921ccf
tan <13967345025@163.com>
2017-07-21 08:50:17 +0800

    登录页面基本实现

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,8 +1,25 @@
+//渲染头像和姓名
+$(function () {
+  //判定是否在登录页面
+  if (location.pathname != "/dashboard/login") {
+    //判定用户是否登入
+    if (!$.cookie("PHPSESSID")) {
+      location.href = "/dashboard/login";
+      return false;
+    }
+    var userInfo = JSON.parse($.cookie("userInfo"));
+    var html = template("tpl1", userInfo);
+    $("#profile").html(html);
+  }
 
-	NProgress.start();
+  NProgress.start();
 
-	NProgress.done();
+  NProgress.done();
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+  $('.navs ul').prev('a').on('click', function () {
+    $(this).next().slideToggle();
+  });
+
+
+
+});
